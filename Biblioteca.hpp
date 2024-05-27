ba15919a856972fee0b82208e485eebe1b9ab53c
DanielAndrade19 <danielopes191204@gmail.com>
2024-05-27 10:41:18 -0300

    Biblioteca.hpp

--- Biblioteca.hpp
+++ Biblioteca.hpp
@@ -7,5 +7,17 @@
 class Biblioteca { 
 
           private:
+                   string localizarLivro (int);
+                   string multar(int); // recebe o statusEmprestimo, valor da multa e muda o status 
+                   string editarPropriedades(string,int,int); // condicao do livro, status do livro e status do emprestimo
                    
-                  
+                   string acervo(string);
+                   string cadastrarLivro(int,string,string,int,string,string,int,string); // dados do livro 
+                   string receberLivro(int);
+                   string cadastroCliente(int;string;string;int;string);// dados do cliente
+                   string receberPagamento(int); // status da multa
+                   
+          public:  
+                  string enviarNotificacao();
+                  string enviarCobranca();
+                   
