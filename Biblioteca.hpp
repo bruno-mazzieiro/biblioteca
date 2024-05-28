d8e1cb1f4b00064c01ac04846251f8d423ca5a61
juliacamelo <74781632+juliacamelo@users.noreply.github.com>
2024-05-28 09:24:41 -0300

    Update Biblioteca.hpp

--- Biblioteca.hpp
+++ Biblioteca.hpp
@@ -7,11 +7,11 @@
 class Biblioteca { 
 
           private:
-                   string localizarLivro (int);
-                   string multar(int); // recebe o statusEmprestimo, valor da multa e muda o status 
-                   string editarPropriedades(string,int,int); // condicao do livro, status do livro e status do emprestimo
-                   
-                   string acervo(string);
+                   string localizarLivro (int);// percorrer a string até identificar a localização
+                   string multar(int); // recebe o statusEmprestimo, informa o valor da multa e muda o status (1 real por dia de atraso)
+                   string editarPropriedades(string,int,int); // condicao do livro, status do livro (disponivel ou emprestado) 
+                    //e status do emprestimo (pagamento atrasado etc)
+                   string acervo(string);//mostrar o titulo dos livros
                    string cadastrarLivro(int,string,string,int,string,string,int,string); // dados do livro 
                    string receberLivro(int);
                    string cadastroCliente(int;string;string;int;string);// dados do cliente
