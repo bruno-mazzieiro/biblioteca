ad431ec0d1f5db6e66ee4c146b780a79fbb16c6b
DanielAndrade19 <danielopes191204@gmail.com>
2024-05-28 09:39:36 -0300

    Create Biblioteca.cpp

---
+++ Biblioteca.cpp
@@ -0,0 +1,22 @@
+          private:                  
+                   string localizarLivro (int);
+                   // Percorrer a string ate identificar a posicao com a mesma id inserida pelo usuario
+                   // pedir a id, for,  
+                   string multar(int); 
+                   // recebe status,recebe data, calcula os dias de atraso, calcula a multa, muda o status do emprestimo para atrasado 
+       // if existe no acervo-> editar propriedade/ else n'ao existe -> cadastra o livro
+                   string editarPropriedades(string,int,int); 
+                   // 
+                   string cadastrarLivro(int,string,string,int,string,string,int,string); 
+                  // criar id
+                   string acervo(string);
+                   string receberLivro(int);
+                   string cadastroCliente(int;string;string;int;string);
+                   string receberPagamento(int); 
+                   
+          public:  
+                  string enviarNotificacao();
+                  string enviarCobranca();
+
+
+ // para o tamanho da biblioteca, consderar o maior num de id 
