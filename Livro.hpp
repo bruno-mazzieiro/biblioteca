07126510866363935792897de72753ee5d3b785f
juliacamelo <74781632+juliacamelo@users.noreply.github.com>
2024-07-05 18:12:24 -0300

    Update Livro.hpp

--- Livro.hpp
+++ Livro.hpp
@@ -8,25 +8,25 @@ class Livro {
 	
 	private:
 		int identificadorLivro;
-		string tituloLivro;
-		string autorLivro;
+		std::string tituloLivro;
+		std::string autorLivro;
 		int anoPublicacao;
-		string generoLivro;
-		string condicaoLivro;
+		std::string generoLivro;
+		std::string condicaoLivro;
 		int statusLivro;				//0 - Disponivel; 1 - Emprestado; 2 - Em processo de devolucao; 3 - Outro
-		string localizacaoLivro;		
+		std::string localizacaoLivro;		
 		
 	public:
 		//Construtores
-		Livro (int, string, string, int, string, string, int, string);
-		Livro (int, string, string, int, string);
+		Livro (int, std::string, std::string, int, std::string, std::string, int, std::string);
+		Livro (int, std::string, std::string, int, std::string);
 		Livro (int);
 
 		//Getters
 		int retornarIdentificador();
-		string retornarStatus (int);
-		string retornarLocalizacao(int);
-		string retornarCondicao (int);
+		std::string retornarStatus (int);
+		std::string retornarLocalizacao(int);
+		std::string retornarCondicao (int);
 	
 };
 
